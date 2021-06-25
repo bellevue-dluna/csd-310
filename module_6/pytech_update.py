7f6a21e4118da8f6915ba7da6ba41fcfc9e833d4
Denae Luna <dmluna@my365.bellevue.edu>
2021-06-24 17:50:41 -0700

    include module_6 dir and pytech update doc.

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -1,6 +1,6 @@
 print("Denae Luna")
 print("CSD 310")
-print("20 June 2021")
+print("24 June 2021")
 
 import json
 # establish connection
@@ -13,6 +13,9 @@ db = client.pytech
 # creates docs collection 
 docs = db.students.find({})
 
+new = db.students.find_one_and_update({"student_id": "1007"}, {"$set": {"last_name": "Johnson"}})
+
+
 print("\n\n-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
 for doc in docs:
     sid = doc.get("student_id")
@@ -22,14 +25,18 @@ for doc in docs:
     print(f"First Name: {fn}")
     print(f"Last Name: {ln}\n")
 
-print("\n\n-- DISPLAYING STUDENTS DOCUMENTS FROM find_one() QUERY --")
+doc = db.students.find_one_and_update({"student_id": "1007"}, {"$set": {"last_name": "Smith"}})
+old = doc.get("last_name")
 doc = db.students.find_one({"student_id": "1007"})
-print(doc)
+new = doc.get("last_name")
+
+print(f"Student with ID of 1007 last name updated from {old} to {new}.")
+
+print("\n\n-- DISPLAYING STUDENTS DOCUMENT 1007 --")
+doc = db.students.find_one({"student_id": "1007"})
 sid = doc.get("student_id")
 fn = doc.get("first_name")
 ln = doc.get("last_name")
 print(f"Student ID: {sid}")
 print(f"First Name: {fn}")
 print(f"Last Name: {ln}")
-
-print("\n\n\tEnd of Program, press any key to exit...")
